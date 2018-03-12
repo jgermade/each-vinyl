b42c6388f81ce00aa7e881de81e29d693f507a0d
Jesús Germade <jesus@germade.es>
2018-03-12 22:23:58 +0100

    tweaks

--- each-vinyl.js
+++ each-vinyl.js
@@ -4,7 +4,9 @@ const through = require('through2');
 const Buffer = require('safe-buffer').Buffer;
 
 module.exports = function eachVinyl (plugin_name, fn) {
-	return function compile(data) {
+	return function () {
+		var args = arguments;
+
 		return through.obj(function (file, enc, cb) {
 			if( file.isNull() ) {
 				cb(null, file);
@@ -17,7 +19,7 @@ module.exports = function eachVinyl (plugin_name, fn) {
 			}
 
 			try {
-				fn.call(this, file);
+				fn.apply( this, [file].concat(args) );
 				this.push(file);
 			} catch (err) {
 				this.emit('error', new PluginError(plugin_name, err, { fileName: file.path }));
